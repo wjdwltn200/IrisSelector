f8b92d983a5d3682b67404cd2471d780d24c4fb5
JeongDongBaek <33086479+JeongDongBaek@users.noreply.github.com>
2018-09-27 16:23:01 +0900

    정민욱
    푸쉬

--- 20180927/20180820/editorScene.h
+++ 20180927/20180820/editorScene.h
@@ -8,6 +8,13 @@ private :
 	HWND	m_hBtnLoad;
 	HWND	m_hBtnSelectTerrain;
 	HWND	m_hBtnSelectObject;
+	HWND	m_hBtnEraser;
+
+	image*	m_pTileSet;
+	image*	m_pObjSet;
+
+	tagTile m_pTiles[TILE_X * TILE_Y];
+	
 
 public:
 	editorScene();
