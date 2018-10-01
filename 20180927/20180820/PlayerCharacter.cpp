46eff4c0727ba87995dfdd474b379f1768be9aee
sunwl1 <sunwl123@naver.com>
2018-10-01 16:12:09 +0900

    이순현
    예외처리 + 캐릭터 밑작업

--- 20180927/20180820/PlayerCharacter.cpp
+++ 20180927/20180820/PlayerCharacter.cpp
@@ -1,6 +1,47 @@
 #include "stdafx.h"
+#include "animation.h"
 #include "PlayerCharacter.h"
 
+
+HRESULT PlayerCharacter::init()
+{
+	img_player = IMAGEMANAGER->addImage("player", "image/resources/player_image/BG_Player_idle_0.bmp", 128, 54, 4, 1, true, RGB(255, 0, 255), m_fX, m_fY);
+	img_leftRun = IMAGEMANAGER->addImage("player_Left_Run", "image/resources/player_image/BG_Player_L_Run.bmp", 342, 54, 6, 1, true, RGB(255, 0, 255), m_fX, m_fY);
+	img_rightRun = IMAGEMANAGER->addImage("player_Right_Run", "image/resources/player_image/BG_Player_R_Run.bmp", 342, 54, 6, 1, true, RGB(255, 0, 255), m_fX, m_fY);
+	
+	return S_OK;
+}
+
+void PlayerCharacter::release()
+{
+
+}
+
+void PlayerCharacter::update()
+{
+	
+	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+	{
+		m_fX -= m_pSpeed;
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+	{
+		m_fX += m_pSpeed;
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_UP))
+	{
+		m_fY -= m_pSpeed;
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+	{
+		m_fY += m_pSpeed;
+	}
+}
+
+void PlayerCharacter::render(HDC hdc)
+{
+	img_player->frameRender(hdc, m_fX, m_fY, 1, 1);
+}
 
 PlayerCharacter::PlayerCharacter()
 {
